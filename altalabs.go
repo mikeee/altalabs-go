d3101b6802ddc5b6dd2a62bec65aa061a3d9f47f
Mike Nguyen <hey@mike.ee>
2024-11-23 22:15:16 +0000

    feat: add srp config
    Signed-off-by: Mike Nguyen <hey@mike.ee>

--- altalabs.go
+++ altalabs.go
@@ -2,8 +2,18 @@ package altalabs
 
 import "net/http"
 
+type Method int
+
+const (
+	METHOD_TOKEN Method = iota
+	METHOD_SRP
+)
+
 type Config struct {
-	Token string
+	Method
+	Token    string
+	Username string
+	Password string
 }
 
 func NewConfig() *Config {
@@ -11,7 +21,15 @@ func NewConfig() *Config {
 }
 
 func (c *Config) WithToken(token string) *Config {
+	c.Method = METHOD_TOKEN
 	c.Token = token
+	return c
+}
+
+func (c *Config) WithSRPAuth(username, password string) *Config {
+	c.Method = METHOD_SRP
+	c.Username = username
+	c.Password = password
 	return c
 }
 
